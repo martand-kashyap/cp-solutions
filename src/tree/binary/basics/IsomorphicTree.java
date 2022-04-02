4d873ed2f2a8e3fb36f969ece515426d055f5fac
Martand Kashyap <kashyapmartand@gmail.com>
2022-04-02 15:15:13 +0530

    isomorphic tree

---
+++ src/tree/binary/basics/IsomorphicTree.java
@@ -0,0 +1,56 @@
+package tree.binary.basics;
+
+import tree.binary.MyTreeNode;
+
+class IsomorphicTree {
+    public static void main(String[] args) {
+        /*-
+            tree 1                  tree 2
+               1                        1
+             /   \                    /    \
+            2     3                  3      2
+           / \   /                    \    / \
+          4   5  6                     6  4   5
+             / \                             / \
+            7   8                           8   7
+         */
+
+        MyTreeNode root1 = new MyTreeNode(1);
+        root1.left = new MyTreeNode(2);
+        root1.right = new MyTreeNode(3);
+
+        root1.left.left = new MyTreeNode(4);
+        root1.left.right = new MyTreeNode(5);
+        root1.right.left = new MyTreeNode(6);
+
+        root1.left.right.left = new MyTreeNode(7);
+        root1.left.right.right = new MyTreeNode(8);
+
+        MyTreeNode root2 = new MyTreeNode(1);
+        root2.left = new MyTreeNode(3);
+        root2.right = new MyTreeNode(2);
+
+        root2.left.right = new MyTreeNode(6);
+        root2.right.left = new MyTreeNode(4);
+        root2.right.right = new MyTreeNode(5);
+
+        root2.right.right.left = new MyTreeNode(8);
+        root2.right.right.right = new MyTreeNode(7);
+
+        System.out.println("Are trees isomorphic? : " + isIsomorphic(root1, root2));
+    }
+
+    private static boolean isIsomorphic(MyTreeNode root1, MyTreeNode root2) {
+        if (root1 == null && root2 == null)
+            return true;
+
+        if (root1 == null || root2 == null)
+            return false;
+
+        boolean isDataSame = root1.val == root2.val;
+
+        return isDataSame &&
+                ((isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right)) ||
+                        (isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left)));
+    }
+}
