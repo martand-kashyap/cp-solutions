11723aa17ec9f610e24fc175379afbddb5735ec5
Martand Kashyap <kashyapmartand@gmail.com>
2022-05-19 23:12:17 +0530

    reverse ll

--- src/linkedlist/ReverseLL.java
+++ src/linkedlist/ReverseLL.java
@@ -2,11 +2,18 @@ package linkedlist;
 
 class ReverseLL {
     public static void main(String[] args) {
+        ListNode head = new ListNode(10);
+        head.next = new ListNode(20);
+        head.next.next = new ListNode(30);
 
+        ReverseLL rev = new ReverseLL();
+        head = rev.reverseList(head);
+
+        LLHelper.print(head);
     }
 
     private ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null)
+        if (head == null || head.next == null)
             return head;
 
         ListNode smaller = reverseList(head.next);
