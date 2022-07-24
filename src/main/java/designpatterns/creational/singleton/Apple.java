99eb91e482d8fead3804fe8b5349e91b7267cd38
Martand <kashyapmartand@gmail.com>
2022-07-24 20:51:20 +0530

    singleton

---
+++ src/main/java/designpatterns/creational/singleton/Apple.java
@@ -0,0 +1,27 @@
+package designpatterns.creational.singleton;
+
+class Apple {
+	private static volatile Apple appleInstance;
+
+	private Apple() {
+
+	}
+
+	public static Apple getInstance() {
+		if (appleInstance == null) {
+			appleInstance = new Apple();
+		}
+		return appleInstance;
+	}
+
+	public static synchronized Apple getInstanceThreadSafe() {
+		if (appleInstance == null) {
+			synchronized (Apple.class) {
+				if (appleInstance == null) {
+					appleInstance = new Apple();
+				}
+			}
+		}
+		return appleInstance;
+	}
+}
