b28982ac42ebf19c990b3d62c0e1ef707bcab8fe
Martand <kashyapmartand@gmail.com>
2022-08-02 08:19:37 +0530

    check parenthesis

---
+++ src/main/java/stack/BalancedParenthesisChecker.java
@@ -0,0 +1,41 @@
+package stack;
+
+import java.io.PrintWriter;
+import java.util.Stack;
+
+class BalancedParenthesisChecker {
+    public static void main(String[] args) {
+        String input = "[(])";
+
+        BalancedParenthesisChecker bpc = new BalancedParenthesisChecker();
+        String res = "Input is balanced? : " + bpc.isExpressionBalanced(input);
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println(res);
+        pw.close();
+    }
+
+    private boolean isExpressionBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i += 1) {
+            char ic = input.charAt(i);
+            if (ic == '(' || ic == '{' || ic == '[')
+                stack.push(ic);
+            else {
+                if (stack.empty())
+                    return false;
+                char top = stack.peek();
+                if ((ic == ')' && top != '(') ||
+                        (ic == '}' && top != '{') ||
+                        (ic == ']' && top != '[')) {
+                    return false;
+                }
+                else
+                    stack.pop();
+            }
+        }
+
+        return stack.empty();
+    }
+}
