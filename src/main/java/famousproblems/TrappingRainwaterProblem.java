d945af80dad0d043ac4eb87cdae5c6035269a426
Martand <kashyapmartand@gmail.com>
2022-08-05 21:23:03 +0530

    rainwater trapping problem (dp)

---
+++ src/main/java/famousproblems/TrappingRainwaterProblem.java
@@ -0,0 +1,47 @@
+package famousproblems;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+class TrappingRainwaterProblem {
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 0, 2, 0, 4};
+
+        TrappingRainwaterProblem trp = new TrappingRainwaterProblem();
+        String res = "Total trapped rainwater (DP approach): " + trp.totalRainwaterTrappedDP(arr) + "\nTotal trapped rainwater (2 pointer approach): " + trp.totalRainwaterTrappedTwoPointer(arr);
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println(res);
+        pw.close();
+    }
+
+    private long totalRainwaterTrappedDP(int[] heights) {
+        long[] waterTrappedOnBuilding = new long[heights.length];
+        int[] maxOnRight = new int[heights.length];
+        int[] maxOnLeft = new int[heights.length];
+        long totalWaterTrapped;
+
+        // calculate maxOnLeft[i]
+        maxOnLeft[0] = heights[0];
+        for (int i = 1; i < heights.length; i++) {
+            maxOnLeft[i] = Integer.max(maxOnLeft[i - 1], heights[i]);
+        }
+
+        // calculate maxOnLeft[i]
+        maxOnRight[heights.length - 1] = heights[heights.length - 1];
+        for (int i = heights.length - 2; i >= 0; i--) {
+            maxOnRight[i] = Integer.max(maxOnRight[i + 1], heights[i]);
+        }
+
+        for (int i = 0; i < heights.length; i++) {
+            waterTrappedOnBuilding[i] = Integer.min(maxOnRight[i], maxOnLeft[i]) - heights[i];
+        }
+
+        totalWaterTrapped = Arrays.stream(waterTrappedOnBuilding).sum();
+        return totalWaterTrapped;
+    }
+
+    private int totalRainwaterTrappedTwoPointer(int[] heights) {
+        return -1;
+    }
+}
