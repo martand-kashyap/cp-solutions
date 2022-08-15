8c37f54c535c51c99dd42775230c35b85a0b6721
Martand <kashyapmartand@gmail.com>
2022-08-15 12:46:19 +0530

    rainwater trapping problem statement

--- src/main/java/famousproblems/TrappingRainwaterProblem.java
+++ src/main/java/famousproblems/TrappingRainwaterProblem.java
@@ -4,6 +4,10 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 
 class TrappingRainwaterProblem {
+    /*-
+    Given an array of N non-negative integers arr[] representing an elevation map where the width of each bar is 1,
+    compute how much water it is able to trap after raining.
+     */
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
 
