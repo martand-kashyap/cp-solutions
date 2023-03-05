7674f3ef143e8f2a62a44ca416b78bed84bee802
Martand <kashyapmartand@gmail.com>
2023-03-05 20:03:00 +0530

    Dutch National Flag Problem

--- src/main/java/famousproblems/DutchNationalFlagProblem.java
+++ src/main/java/famousproblems/DutchNationalFlagProblem.java
@@ -1,5 +1,8 @@
 package famousproblems;
 
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 class DutchNationalFlagProblem {
     /*-
     Given an array nums with n objects colored red, white, or blue,
@@ -21,6 +24,78 @@ class DutchNationalFlagProblem {
     Output: [0,1,2]
      */
     public static void main(String[] args) {
-        
+        int[] nums = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+
+        DutchNationalFlagProblem p = new DutchNationalFlagProblem();
+
+        String res = "The input array \n" + Arrays.toString(nums) + "\n" +
+                "after sorting in place results in" + "\n" +
+                "using counts: " + Arrays.toString(p.usingCounts(nums)) + "\n" +
+                "using pointers: " + Arrays.toString(p.using2Pointers(nums));
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println(res);
+        pw.close();
+    }
+
+    private int[] usingCounts(int[] nums) {
+        int zeroCount = 0, oneCount = 0, twoCount = 0;
+
+        for (int x : nums) {
+            if (x == 0) {
+                zeroCount++;
+            } else if (x == 1) {
+                oneCount++;
+            } else {
+                twoCount++;
+            }
+        }
+
+        int i = 0;
+        while (zeroCount > 0) {
+            nums[i++] = 0;
+            zeroCount--;
+        }
+        while (oneCount > 0) {
+            nums[i++] = 1;
+            oneCount--;
+        }
+        while (twoCount > 0) {
+            nums[i++] = 2;
+            twoCount--;
+        }
+
+        return nums;
+    }
+
+    private int[] using2Pointers(int[] nums) {
+        int left = 0, right = nums.length - 1, current = 0, temp;
+
+        while (current <= right) {
+            switch (nums[current]) {
+                case 0:
+                    temp = nums[left];
+                    nums[left] = nums[current];
+                    nums[current] = temp;
+
+                    left += 1;
+                    current += 1;
+
+                    break;
+                case 1:
+                    current += 1;
+                    break;
+                case 2:
+                    temp = nums[right];
+                    nums[right] = nums[current];
+                    nums[current] = temp;
+
+                    right -= 1;
+
+                    break;
+            }
+        }
+
+        return nums;
     }
 }
