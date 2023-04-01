5283f6a99ed9342390f0a749d821430f8d8cfaad
Martand <kashyapmartand@gmail.com>
2023-04-01 13:04:15 +0530

    binary search: update find the min ele in sorted rotated array

---
+++ src/main/java/search/binary/SortedRotatedArrayFindPivot.java
@@ -0,0 +1,41 @@
+package search.binary;
+
+import java.io.PrintWriter;
+
+class SortedRotatedArrayFindPivot {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 8, 9, 10, 1, 2, 3};
+        int n = arr.length;
+
+        SortedRotatedArrayFindPivot findPivot = new SortedRotatedArrayFindPivot();
+        String res = "The array is rotated at index " + findPivot.findPivotIndex(arr, n);
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println(res);
+
+        pw.close();
+    }
+
+    int findPivotIndex(int[] nums, int n) {
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int midIdx = left + (right - left) / 2;
+            int prevIdx = (midIdx - 1 + n) % n;
+            int nextIdx = (midIdx + 1) % n;
+
+            if (nums[prevIdx] >= nums[midIdx] && nums[midIdx] <= nums[nextIdx]) {
+                return nums[midIdx];
+            }
+            //it is important to check if the right half is sorted or not before checking the left half
+            else if (nums[midIdx] <= nums[right]) {
+                right = midIdx - 1;
+            }
+            //
+            else if (nums[left] <= nums[midIdx]) {
+                left = midIdx + 1;
+            }
+        }
+
+        return -1;
+    }
+}
