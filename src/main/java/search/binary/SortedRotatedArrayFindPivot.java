577aa3a7fbc9909e590d11092392de24cfd5e5ac
Martand <kashyapmartand@gmail.com>
2023-04-01 20:08:10 +0530

    binary search: update search in sorted rotated array

--- src/main/java/search/binary/SortedRotatedArrayFindPivot.java
+++ src/main/java/search/binary/SortedRotatedArrayFindPivot.java
@@ -8,7 +8,7 @@ class SortedRotatedArrayFindPivot {
         int n = arr.length;
 
         SortedRotatedArrayFindPivot findPivot = new SortedRotatedArrayFindPivot();
-        String res = "The array is rotated at index " + findPivot.findPivotIndex(arr, n);
+        String res = "The array is rotated at element " + findPivot.findPivotElementAltImpl2(arr, n);
 
         PrintWriter pw = new PrintWriter(System.out);
         pw.println(res);
@@ -16,7 +16,46 @@ class SortedRotatedArrayFindPivot {
         pw.close();
     }
 
-    int findPivotIndex(int[] nums, int n) {
+    private int findPivotElement(int[] nums, int n) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int l = 0, r = n - 1;
+
+        // If the first element is less than the last element then there is no rotation. The first element is minimum.
+        if (nums[l] <= nums[r]) {
+            return nums[l];
+        }
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            // If the middle element is smaller than its previous element, then it is the minimum element
+            if (m > 0 && nums[m] < nums[m - 1]) {
+                return nums[m];
+            }
+
+            // If the middle is greater than its next element, then the next element is the minimum element
+            if (m < n - 1 && nums[m] > nums[m + 1]) {
+                return nums[m + 1];
+            }
+
+            if (nums[l] <= nums[m]) { // left array is sorted. So the pivot is on the right side
+                l = m + 1;
+            } else { //right array is sorted. So the pivot is on the left side
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+
+    /*
+    //fails some scenarios
+    private int findPivotElementAltImpl1(int[] nums, int n) {
         int left = 0, right = n - 1;
         while (left <= right) {
             int midIdx = left + (right - left) / 2;
@@ -24,17 +63,49 @@ class SortedRotatedArrayFindPivot {
             int nextIdx = (midIdx + 1) % n;
 
             if (nums[prevIdx] >= nums[midIdx] && nums[midIdx] <= nums[nextIdx]) {
-                return midIdx;
+                return nums[midIdx];
             }
             //it is important to check if the right half is sorted or not before checking the left half
             else if (nums[midIdx] <= nums[right]) {
                 right = midIdx - 1;
-            }
-            else if (nums[left] <= nums[midIdx]) {
+            } else if (nums[left] <= nums[midIdx]) {
                 left = midIdx + 1;
             }
         }
 
         return -1;
+    }*/
+
+    private int findPivotElementAltImpl2(int[] nums, int n) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            //Case 1: The leftmost value is less than the rightmost value in the array. This means that the array is not rotated.
+            //Example: [1 2 3 4 5 6 7 8 9]
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                //Case 2: The value in the middle of the array is greater than the leftmost and rightmost values in the array.
+                //Example: [4 5 6 7 |8| 9 1 2 3]
+                left = mid + 1;
+            } else {
+                //Case 3: The value in the middle of the array is less than the leftmost and rightmost values in the array.
+                //Example: [7 8 9 1 |2| 3 4 5 6]
+                right = mid;
+            }
+        }
+
+        return nums[left];
     }
 }
--- src/main/java/search/binary/SortedRotatedArraySearch.java
+++ src/main/java/search/binary/SortedRotatedArraySearch.java
@@ -4,7 +4,7 @@ import java.io.PrintWriter;
 
 class SortedRotatedArraySearch {
     public static void main(String[] args) {
-        int[] arr = {3, 1};
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
         int searchKey = 1;
 
         SortedRotatedArraySearch obj = new SortedRotatedArraySearch();
@@ -19,8 +19,8 @@ class SortedRotatedArraySearch {
     private int searchInRotatedSortedArray(int[] nums, int searchKey) {
         int p = findPivotIndex(nums, nums.length);
 
-        int idx1 = orderAgnosticBinarySearch(nums, 0, p, searchKey);
-        int idx2 = orderAgnosticBinarySearch(nums, p + 1, nums.length - 1, searchKey);
+        int idx1 = binarySearchI(nums, 0, p - 1, searchKey);
+        int idx2 = binarySearchI(nums, p, nums.length - 1, searchKey);
 
         if (idx1 == -1 && idx2 == -1)
             return -1;
@@ -30,54 +30,74 @@ class SortedRotatedArraySearch {
 
     private int findPivotIndex(int[] nums, int n) {
         int left = 0, right = n - 1;
+
+        while (left < right) {
+            //Case 1: The leftmost value is less than the rightmost value in the array. This means that the array is not rotated.
+            //Example: [1 2 3 4 5 6 7 8 9]
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                //Case 2: The value in the middle of the array is greater than the leftmost and rightmost values in the array.
+                //Example: [4 5 6 7 |8| 9 1 2 3]
+                left = mid + 1;
+            } else {
+                //Case 3: The value in the middle of the array is less than the leftmost and rightmost values in the array.
+                //Example: [7 8 9 1 |2| 3 4 5 6]
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    private int binarySearchI(int[] arr, int l, int u, int searchKey) {
+        while (l <= u) {
+            int m = l + (u - l) / 2;
+            if (searchKey < arr[m])
+                u = m - 1;
+            else if (searchKey > arr[m])
+                l = m + 1;
+            else
+                return m;
+        }
+        return -1;
+    }
+
+    private int solveInOnePass(int[] nums, int searchKey) {
+        int n = nums.length, left = 0, right = n - 1;
+
         while (left <= right) {
-            int midIdx = left + (right - left) / 2;
-            int prevIdx = (midIdx - 1 + n) % n;
-            int nextIdx = (midIdx + 1) % n;
+            int mid = left + (right - left) / 2;
 
-            if (nums[prevIdx] >= nums[midIdx] && nums[midIdx] <= nums[nextIdx]) {
-                return midIdx;
+            if (nums[mid] == searchKey) {
+                return mid;
             }
-            //it is important to check if the right half is sorted or not before checking the left half
-            else if (nums[midIdx] <= nums[right]) {
-                right = midIdx - 1;
-            } else if (nums[left] <= nums[midIdx]) {
-                left = midIdx + 1;
+            //is the left half sorted?
+            else if (nums[left] <= nums[mid]) {
+                //left half is indeed sorted
+                //is the target within the sorted bounds?
+                if (nums[left] <= searchKey && searchKey < nums[mid]) {
+                    //yes
+                    right = mid - 1;
+                } else {
+                    //no
+                    left = mid + 1;
+                }
+            } else {
+                //left half is not sorted => right half MUST be sorted
+                //is the target within the sorted bounds?
+                if (nums[mid] < searchKey && searchKey <= nums[right]) {
+                    //yes
+                    left = mid + 1;
+                } else {
+                    //no
+                    right = mid - 1;
+                }
             }
-        }
-
-        return -1;
-    }
-
-    private int orderAgnosticBinarySearch(int[] arr, int l, int r, int searchKey) {
-        if (arr[0] < arr[1])
-            return binarySearchAsc(arr, l, r, searchKey);
-        else
-            return binarySearchDsc(arr, l, r, searchKey);
-    }
-
-    private int binarySearchAsc(int[] arr, int l, int u, int searchKey) {
-        while (l <= u) {
-            int m = l + (u - l) / 2;
-            if (searchKey < arr[m])
-                u = m - 1;
-            else if (searchKey > arr[m])
-                l = m + 1;
-            else
-                return m;
-        }
-        return -1;
-    }
-
-    private int binarySearchDsc(int[] arr, int l, int u, int searchKey) {
-        while (l <= u) {
-            int m = l + (u - l) / 2;
-            if (searchKey < arr[m])
-                l = m + 1;
-            else if (searchKey > arr[m])
-                u = m - 1;
-            else
-                return m;
         }
         return -1;
     }
